8efe60e413824bfb85d955e1fa6ed088ea3d0d1e
Samrat <thapa_samrat1@outlook.com>
2022-06-23 14:21:39 +0545

    parser done

---
+++ parser/parser_table.py
@@ -0,0 +1,20 @@
+
+import pandas as pd
+with open ("data.txt", "r") as myfile:
+    data = ' '.join([line for line in myfile.readlines()])
+
+list_data=data.replace('    ','|').split("\n")
+list_data_clean = list()
+for row in list_data:
+   
+    if "State" in row or "Abbr." in row or "Code" in row or row == "":
+        continue
+    else:
+        list_data_clean.append(row.split('|')[0:3])
+        if len(row) > 19:
+            list_data_clean.append(row.split('|')[3:])
+            
+
+df = pd.DataFrame(list_data_clean, columns=["State","Postal-abbr.","Postal Code"])
+df = df.sort_values("Postal Code")
+df.to_csv("output.csv")
